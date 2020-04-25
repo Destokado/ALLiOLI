ca82d890a2f3a2f17e329ab00188157250d6da4f
gillemp <guplem@gmail.com>
2020-04-25 21:25:57 +0200

    Basic foot placement

--- ALLiOLI/Assets/Scripts/Character/IKFootPlacement.cs
+++ ALLiOLI/Assets/Scripts/Character/IKFootPlacement.cs
@@ -18,18 +18,26 @@ public class IKFootPlacement : MonoBehaviour
 
     private void OnAnimatorIK(int layerIndex)
     {
-        anim.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
-        anim.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+        SetFootPosition(AvatarIKGoal.LeftFoot);
+        SetFootPosition(AvatarIKGoal.RightFoot);
+    }
+
+    private void SetFootPosition(AvatarIKGoal foot)
+    {
+        anim.SetIKPositionWeight(foot, 1f);
+        anim.SetIKRotationWeight(foot, 1f);
         
-        //Left foot
         RaycastHit hit;
-        Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot)+Vector3.up*maxFootElevation, Vector3.down);
-        if (Physics.Raycast(ray, out hit, footBoneDistanceToGround + maxFootElevation, walkableLayers))
+        Ray ray = new Ray(anim.GetIKPosition(foot)+Vector3.up*maxFootElevation, Vector3.down);
+        if (Physics.Raycast(ray, out hit, Vector3.Distance(anim.GetIKPosition(foot), Vector3.up*maxFootElevation), walkableLayers))
         {
             Debug.DrawLine(ray.origin, hit.point, Color.red);
             Vector3 footPosition = hit.point;
             footPosition.y += footBoneDistanceToGround;
-            anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
+            anim.SetIKPosition(foot, footPosition);
+            anim.SetIKRotation(foot,Quaternion.LookRotation(transform.forward, hit.normal));
+        } else {
+            Debug.DrawRay(ray.origin, ray.direction, Color.green);
         }
     }
 }
