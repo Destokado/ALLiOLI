2b0172056f579ac2319e9ddbbdbf9da8cadcd10d
Guillem Poy <guplem@gmail.com>
2020-06-01 19:41:57 +0200

    Bomb and basic spawn traps

---
+++ ALLiOLI/Assets/Scripts/TrapRelated/BombTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombTrap : Trap
+{
+    private SimpleAnimationsManager animManager;
+
+    private void Awake()
+    {
+        animManager = gameObject.GetComponent<SimpleAnimationsManager>();
+    }
+
+    protected override void Reload()
+    {
+        animManager.GetAnimation(0).mirror = true;
+        animManager.Play(0);
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+        animManager.GetAnimation(0).mirror = false;
+        animManager.Play(0);
+    }
+}
