a0d36a4ece3b11b0c4813211cb2a347403fb3178
Guillem Poy <guplem@gmail.com>
2020-06-01 11:30:30 +0200

    Basic new pre lobby

---
+++ ALLiOLI/Assets/Scripts/PreLobbyGui.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PreLobbyGui : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField ipTextField;
+
+    public void Host()
+    {
+        Debug.Log("Starting to host a match");
+        AllIOliNetworkManager.singleton.StartHost();
+    }
+    
+    public void JoinAsClient()
+    {
+        string text = ipTextField.text;
+        if (text.IsNullOrEmpty())
+            text = AllIOliNetworkManager.singleton.networkAddress;
+        
+        Debug.Log($"Joining '{text}'");
+        AllIOliNetworkManager.singleton.networkAddress = text;
+        AllIOliNetworkManager.singleton.StartClient();
+    }
+}
