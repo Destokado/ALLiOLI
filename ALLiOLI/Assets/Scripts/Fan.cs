376047531ae156d11922bc3d678a6c1ca4495989
Guillem Poy <guplem@gmail.com>
2020-06-09 18:48:04 +0200

    Fan

---
+++ ALLiOLI/Assets/Scripts/Fan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField] private float maxEffectDistance = 3f;
+    [SerializeField] private float force = 50f;
+    [SerializeField] private AnimationCurve forceByDistance;
+
+    private void OnTriggerStay(Collider other)
+    {
+        float distance = Vector3.Distance(transform.position, other.transform.position);
+        float forcePerByDistance = forceByDistance.Evaluate(1 - (distance / maxEffectDistance));
+        other.attachedRigidbody.AddForce(transform.forward * (force * forcePerByDistance), ForceMode.Impulse);
+        Debug.Log($"ADDING FORCE TO {other.gameObject}");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 position = transform.position;
+        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(position, maxEffectDistance);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(position, position+transform.forward*force);
+    }
+}
