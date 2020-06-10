b60983cea10a3e8bdb3046b31bc39ee23aab49d8
Guillem Poy <guplem@gmail.com>
2020-06-10 18:12:07 +0200

    Simplified dead zone

--- ALLiOLI/Assets/Scripts/DeadZone.cs
+++ ALLiOLI/Assets/Scripts/DeadZone.cs
@@ -7,15 +7,15 @@ public class DeadZone : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Character character = other.GetComponentInParent<Character>();
-        if (!character || character.isDead) return;
-        if (character.hasFlag) //If the character has the flag, reset it
+        if (!character || character.isDead || character.Owner.Client.isLocalClient) return;
+        /*if (character.hasFlag) //If the character has the flag, reset it
         {
             character.hasFlag = false; //Tell the character they no longer have the flag (deactivate placeholder trap)
             
             character.Owner.Flag.Reset(); //reset it
-        }
+        }*/
         Debug.Log($"Killed '{character.name}', was caught trying to get out of the map!'.", gameObject);
         character.Kill(Vector3.zero, character.transform.position);
-        Destroy(other.gameObject, 3f);
+        //Destroy(other.gameObject, 3f);
     }
 }
